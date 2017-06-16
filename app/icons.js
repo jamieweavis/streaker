6e0b7c0459950272334fbfeff4bd782020626de9
Jamie Weavis <jamie.weavis@gmail.com>
2017-06-16 16:45:36 +0100

    :truck: Move macOS tray icons into macOS folder

--- app/icons.js
+++ app/icons.js
@@ -3,8 +3,8 @@
 const path = require('path')
 
 module.exports = {
-  done: path.join(__dirname, 'icons/doneTemplate.png'),
-  todo: path.join(__dirname, 'icons/todoTemplate.png'),
-  load: path.join(__dirname, 'icons/loadTemplate.png'),
-  fail: path.join(__dirname, 'icons/failTemplate.png')
+  done: path.join(__dirname, 'icons/macos/doneTemplate.png'),
+  todo: path.join(__dirname, 'icons/macos/todoTemplate.png'),
+  load: path.join(__dirname, 'icons/macos/loadTemplate.png'),
+  fail: path.join(__dirname, 'icons/macos/failTemplate.png')
 }
