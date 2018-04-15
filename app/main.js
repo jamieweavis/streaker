ff5e9004495644359884a7cfa6e072a1a5023715
Jamie Weavis <jamie.weavis@gmail.com>
2018-04-15 17:12:38 +0100

    Use `contribution` to fetch contribution data

--- app/main.js
+++ app/main.js
@@ -4,15 +4,13 @@ const log = require('./logger')
 const icon = require('./icon')
 const pjson = require('../package.json')
 const store = require('./store')
-const request = require('request-promise-native')
-const DOMParser = require('dom-parser')
 const AutoLaunch = require('auto-launch')
+const contribution = require("contribution");
 
 const electron = require('electron')
 const { app, BrowserWindow, Tray, Menu, shell, ipcMain } = electron
 
 app.on('ready', () => {
-  const parser = new DOMParser()
   const streakerAutoLauncher = new AutoLaunch({
     name: pjson.name,
     path: `/Applications/${pjson.name}.app`
@@ -26,24 +24,11 @@ app.on('ready', () => {
     tray.setContextMenu(createTrayMenu('Loading...'))
 
     let username = store.get('username')
-    let contributionUrl = `https://github.com/users/${username}/contributions`
-    let contributionStreak = 0
-    let contributedToday = false
 
-    request(contributionUrl).then((htmlString) => {
-      const days = parser.parseFromString(htmlString, 'text/html').getElementsByClassName('day')
-      days.forEach((day, index) => {
-        if (day.getAttribute('data-count') > 0) {
-          contributionStreak++
-          contributedToday = true
-        } else {
-          if (days.length - 1 !== index) contributionStreak = 0
-          contributedToday = false
-        }
-      })
-      tray.setContextMenu(createTrayMenu(`Streak: ${contributionStreak}`))
-      tray.setImage(contributedToday ? icon.done : icon.todo)
-      log.info(`Request successful - username=${username} streak=${contributionStreak} today=${contributedToday}`)
+    contribution(username).then(data => {
+      tray.setContextMenu(createTrayMenu(`Streak: ${data.streak}`))
+      tray.setImage(data.streak > 0 ? icon.done : icon.todo)
+      log.info(`Request successful - username=${username} streak=${data.streak} today=${data.contribution > 0}`);
     }).catch((error) => {
       tray.setContextMenu(createTrayMenu('Failed to get streak'))
       tray.setImage(icon.fail)
@@ -57,10 +42,10 @@ app.on('ready', () => {
     let menuTemplate = [
       { label: `${displayLabel} (${username})`, enabled: false },
       { type: 'separator' },
-      { label: 'Refresh', accelerator: 'Cmd+R', click: requestContributionData },
+      { label: 'Reload', accelerator: 'Cmd+R', click: requestContributionData },
       { label: 'Open GitHub Profile...', accelerator: 'Cmd+O', click: () => { shell.openExternal(githubProfileUrl) } },
       { type: 'separator' },
-      { label: 'Set GitHub Username...', click: createUsernameWindow },
+      { label: 'Set GitHub Username...', accelerator: 'Cmd+S', click: createUsernameWindow },
       {
         label: 'Preferences',
         submenu: [{
