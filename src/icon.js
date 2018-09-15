82b36b50cb10b43e25c522259dd98ee1be01aecd
KeziahMoselle <keziahmoselle@protonmail.com>
2018-09-15 11:06:35 +0200

    Export white icon on win32

--- src/icon.js
+++ src/icon.js
@@ -1,8 +1,17 @@
 const path = require('path');
 
-module.exports = {
-  done: path.join(__dirname, 'icons/macos/doneTemplate.png'),
-  todo: path.join(__dirname, 'icons/macos/todoTemplate.png'),
-  load: path.join(__dirname, 'icons/macos/loadTemplate.png'),
-  fail: path.join(__dirname, 'icons/macos/failTemplate.png')
-};
+if (process.platform == 'win32') {
+  module.exports = {
+    done: path.join(__dirname, 'icons/win/doneTemplate.png'),
+    todo: path.join(__dirname, 'icons/win/todoTemplate.png'),
+    load: path.join(__dirname, 'icons/win/loadTemplate.png'),
+    fail: path.join(__dirname, 'icons/win/failTemplate.png')
+  }
+} else {
+  module.exports = {
+    done: path.join(__dirname, 'icons/macos/doneTemplate.png'),
+    todo: path.join(__dirname, 'icons/macos/todoTemplate.png'),
+    load: path.join(__dirname, 'icons/macos/loadTemplate.png'),
+    fail: path.join(__dirname, 'icons/macos/failTemplate.png')
+  }
+}
