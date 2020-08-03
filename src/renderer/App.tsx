aa349cdee12be003d6aa56ffc70f7a7f43d8fa0d
Jamie Weavis <jamie.weavis@gmail.com>
2020-08-03 14:50:03 +0100

    Add primer

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { hot } from 'react-hot-loader';
+import { Avatar, BranchName } from '@primer/components';
 
-const App = (): JSX.Element => <div>Hello World!</div>;
+import store from '../common/store';
+
+const App = (): JSX.Element => (
+  <Fragment>
+    <Avatar
+      square
+      src={`https://avatars.githubusercontent.com/${store.get('username')}`}
+    />
+    <BranchName>{store.get('username')}</BranchName>
+  </Fragment>
+);
 
 export default hot(module)(App);
