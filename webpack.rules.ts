6da654c4d9d27a19721b2e75db77f15528d90243
Jamo <jamie.weavis@gmail.com>
2025-03-14 17:56:23 +0000

    build: downgrade tailwind and remove asset relocator loader

--- webpack.rules.ts
+++ webpack.rules.ts
@@ -8,16 +8,16 @@ export const rules: Required<ModuleOptions>['rules'] = [
     test: /native_modules[/\\].+\.node$/,
     use: 'node-loader',
   },
-  {
-    test: /[/\\]node_modules[/\\].+\.(m?js|node)$/,
-    parser: { amd: false },
-    use: {
-      loader: '@vercel/webpack-asset-relocator-loader',
-      options: {
-        outputAssetBase: 'native_modules',
-      },
-    },
-  },
+  // {
+  //   test: /[/\\]node_modules[/\\].+\.(m?js|node)$/,
+  //   parser: { amd: false },
+  //   use: {
+  //     loader: '@vercel/webpack-asset-relocator-loader',
+  //     options: {
+  //       outputAssetBase: 'native_modules',
+  //     },
+  //   },
+  // },
   {
     test: /\.tsx?$/,
     exclude: /(node_modules|\.webpack)/,
